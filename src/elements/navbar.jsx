a69ffcba87421fad75f60f779631c24d25a85542
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-06-12 15:14:28 -0300

    feat: phone menu bar

--- src/elements/navbar.jsx
+++ src/elements/navbar.jsx
@@ -3,47 +3,98 @@ import '../styles/navbar.css'
 
 export default function Navbar () {
   const [headerStyle, setHeaderStyle] = useState()
+  const [menuOpacity, setMenuOpacity] = useState({ visibility: 'hidden', opacity: 0 })
+  const [menuIsVisible, setMenuVisible] = useState(false)
   window.addEventListener('scroll', () => {
     if (window.scrollY !== 0) {
-      setHeaderStyle({ background: '#18002b8a', borderRadius: '50px', border: '1px solid #ffffff27' })
+      setHeaderStyle({
+        background: '#18002b8a',
+        borderRadius: '50px',
+        border: '1px solid #ffffff27'
+      })
     } else {
       setHeaderStyle({})
     }
   })
 
+  function handleMenu () {
+    if (menuIsVisible) {
+      setMenuOpacity({ visibility: 'hidden', opacity: 0 })
+      setMenuVisible(false)
+    } else {
+      setMenuOpacity({ visibility: 'visible', opacity: 1 })
+      setMenuVisible(true)
+    }
+  }
+
   return (
     <header>
-      <nav id="navbar" style={headerStyle}>
+      <nav id='navbar' style={headerStyle}>
         <div>
-          <h1 onClick={() => {
-            window.location.reload()
-          }}>
+          <h1
+            onClick={() => {
+              window.location.reload()
+            }}
+          >
             DEENE<span>DEV</span>
           </h1>
         </div>
         <div id='navbar-link'>
           <ul>
             <li>
-              <a href="#aboutme">
+              <a href='#aboutme'>
                 ABOUT
-                <hr/>
+                <hr />
               </a>
             </li>
             <li>
-              <a href="#projects">
+              <a href='#projects'>
                 PROJECTS
-                <hr/>
+                <hr />
               </a>
             </li>
             <li>
-              <a href="#technology">
+              <a href='#technology'>
                 TECHNOLOGY
-                <hr/>
+                <hr />
               </a>
             </li>
           </ul>
         </div>
+        <div
+          onClick={() => {
+            handleMenu()
+          }}
+          id='phone-nav-link'
+        >
+          <div id='hamburguer-navlink'>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
       </nav>
+      <div style={menuOpacity} id='phone-menu'>
+        <ul>
+          <li onClick={() => { handleMenu() }}>
+            <a href='#aboutme'>
+              ABOUT
+            </a>
+          </li>
+          <hr/>
+          <li onClick={() => { handleMenu() }}>
+            <a href='#projects'>
+              PROJECTS
+            </a>
+          </li>
+          <hr/>
+          <li onClick={() => { handleMenu() }}>
+            <a href='#technology'>
+              TECHNOLOGY
+            </a>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
