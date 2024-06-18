2df9bd90d181832572d0ac8d1560ca77bcff9375
deenedev <danielfalcaoviktorio@gmail.com>
2024-06-18 15:12:37 -0300

    fix: presentation background

--- src/elements/presentation.jsx
+++ src/elements/presentation.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/presentation.css'
 
 export default function Presentation () {
+  const [mouseY, setMouseY] = useState(0)
+  const [mouseX, setMouseX] = useState(0)
   return (
     <>
-      <section style={{ backgroundColor: '#050505', backgroundImage: 'url("/bg.jpg")', backgroundSize: 'cover', backgroundPosition: 'center center', backgroundRepeat: 'no-repeat' }} id='presentation'>
+      <section onMouseMove={(evt) => {
+          
+        }} style={{ backgroundColor: '#050505', backgroundImage: 'url("/bg.jpg")', backgroundSize: 'cover', backgroundPosition: `${mouseX}px ${mouseY}px`, backgroundRepeat: 'no-repeat' }} id='presentation'>
         <h1 draggable>deenedev</h1>
       </section>
     </>
