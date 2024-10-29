f0673536d06d25114cbabdad98de9ff04e265ccc
Daniel <danielfalcaoviktorio@gmail.com>
2024-10-29 06:07:46 -0300

    feat: scroll animation add to technology section

--- src/elements/technology.jsx
+++ src/elements/technology.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { allTechnologies } from '../data/technologies'
+import ScrollReveal from 'scrollreveal'
 
 export default function Technology () {
+  useEffect(() => {
+    for (const tech of allTechnologies) {
+      ScrollReveal().reveal(`.card${tech.id}`, {
+        duration: 300,
+        delay: 90 * Number(tech.id),
+        distance: '-90px'
+      })
+    }
+  }, [])
   return (
     <>
       <section id='technology'>
