5cbbe395c15f696b5c3f8e1167aa2ba3039bab09
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-06 16:53:08 -0300

    feat: ensure technology section render new technology and change card animation and design

--- src/elements/technology.jsx
+++ src/elements/technology.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { allTechnologies } from '../data/technologies'
 import ScrollReveal from 'scrollreveal'
 
-export default function Technology () {
+export default function Technology() {
   useEffect(() => {
     for (const tech of allTechnologies) {
       ScrollReveal().reveal(`.card${tech.id}`, {
@@ -14,18 +14,31 @@ export default function Technology () {
   }, [])
   return (
     <>
-      <section id='technology'>
-        <div id='technology-title'>
+      <section id="technology">
+        <div id="technology-title">
           <h1>Meu Arsenal</h1>
         </div>
-        <div id='technologies'>
+        <div id="technologies">
           {allTechnologies.map((tech) => {
             return (
-              <div key={tech.id} className={`card card${tech.id}`} id='technology-box' style={{ [`--choosencolor${tech.id}`]: tech.color }}>
-                <span>{tech.name}</span>
-                <div className='content'>
-                  <a rel='noreferrer' target='_blank' aria-label={tech.name} href={tech.url}>
-                    <img src={`./assets/tech/${tech.img}`} alt={`${tech.name}`}/>
+              <div
+                key={tech.id}
+                className={`card card${tech.id}`}
+                id="technology-box"
+                style={{ [`--choosencolor${tech.id}`]: tech.color }}
+              >
+                <span></span>
+                <div className="content">
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    aria-label={tech.name}
+                    href={tech.url}
+                  >
+                    <img
+                      src={`./assets/tech/${tech.img}`}
+                      alt={`${tech.name}`}
+                    />
                   </a>
                 </div>
               </div>
