bd29d1b18e30765a890dbc51c320e794a43f2c6c
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-06 16:53:08 -0300

    feat: ensure technology section render new technology and change card animation and design

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -3,91 +3,84 @@ export const allTechnologies = [
     id: 1,
     name: 'JavaScript',
     img: 'javascript.png',
-    url: 'https://github.com/danielfalcaovt/AngelaYuCourse/tree/main/ADVANCED%20JAVASCRIPT',
     color: '#FFD600'
   },
   {
     id: 2,
     name: 'NodeJS',
     img: 'nodejs.png',
-    url: 'https://github.com/danielfalcaovt/AngelaYuCourse/tree/main/BACKEND',
     color: '#21A366'
   },
   {
     id: 3,
     name: 'React',
     img: 'react.png',
-    url: 'https://github.com/danielfalcaovt/AngelaYuCourse/tree/main/REACT',
     color: '#1976D2'
   },
   {
     id: 4,
     name: 'TypeScript',
     img: 'typescript.png',
-    url: 'https://github.com/danielfalcaovt/Solid-Clean-Architecture-TDD-TypeScript-Training',
     color: '#1976D2'
   },
   {
     id: 5,
     name: 'PostgreSQL',
     img: 'postgresql.png',
-    url: 'https://github.com/danielfalcaovt/SCEA',
     color: '#0177BD'
   },
   {
     id: 6,
     name: 'Bootstrap',
     img: 'bootstrap.png',
-    url: 'https://github.com/danielfalcaovt/AngelaYuCourse/tree/main/BOOTSTRAP',
     color: '#7C4DFF'
   },
   {
     id: 7,
     name: 'C#',
     img: 'csharp.png',
-    url: 'https://github.com/danielfalcaovt/',
     color: '#B14796'
   },
   {
     id: 8,
     name: 'jQuery',
     img: 'jquery.png',
-    url: 'https://github.com/danielfalcaovt/AngelaYuCourse/tree/main/JQUERY/jquery1',
     color: 'white'
   },
   {
     id: 9,
     name: 'Jest',
     img: 'jest.png',
-    url: 'https://github.com/danielfalcaovt/TDD',
     color: '#d32e5a'
   },
   {
     id: 10,
     name: 'Git',
     img: 'git.png',
-    url: 'https://github.com/danielfalcaovt',
     color: '#F05033'
   },
   {
     id: 11,
     name: 'Docker',
     img: 'docker.png',
-    url: 'https://github.com/danielfalcaovt',
     color: '#1D63ED'
   },
   {
     id: 12,
     name: '.NET FRAMEWORK',
     img: 'dotnet.png',
-    url: 'https://github.com/danielfalcaovt',
     color: '#5027D5'
   },
   {
     id: 13,
     name: 'Redis',
     img: 'redis.png',
-    url: 'https://github.com/danielfalcaovt',
     color: '#D9281A'
+  },
+  {
+    id: 14,
+    name: 'NestJS',
+    img: 'nestjs.png',
+    color: '#DF224E'
   }
 ]
