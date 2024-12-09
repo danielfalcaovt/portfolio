fa15c3ae1907062d6aabf8fd1418e680defb9e74
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-09 14:51:47 -0300

    refactor: move technologies to a single element

--- src/components/technology.jsx
+++ src/components/technology.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { allTechnologies } from '../data/technologies'
 import ScrollReveal from 'scrollreveal'
+import Technologies from '../elements/technologies'
 
 export default function Technology () {
   useEffect(() => {
@@ -18,33 +19,7 @@ export default function Technology () {
         <div id="technology-title">
           <h1>Minhas Ferramentas</h1>
         </div>
-        <div id="technologies">
-          {allTechnologies.map((tech) => {
-            return (
-              <div
-                key={tech.id}
-                className={`card card${tech.id}`}
-                id="technology-box"
-                style={{ [`--choosencolor${tech.id}`]: tech.color }}
-              >
-                <span></span>
-                <div className="content">
-                  <a
-                    rel="noreferrer"
-                    target="_blank"
-                    aria-label={tech.name}
-                    href={tech.url}
-                  >
-                    <img
-                      src={`./assets/tech/${tech.img}`}
-                      alt={`${tech.name}`}
-                    />
-                  </a>
-                </div>
-              </div>
-            )
-          })}
-        </div>
+        <Technologies />
       </section>
     </>
   )
