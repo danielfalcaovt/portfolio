a8a46858252ae4370a46cbcb41d1e819d59f348f
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-10 15:38:39 -0300

    refactor: change useState approach to useContext

--- src/components/projects-container.jsx
+++ src/components/projects-container.jsx
@@ -1,24 +1,23 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import ProjectsCarrosel from '../elements/projects-carrosel'
-import ProjectsModal from '../elements/projects-modal'
+import { ModalContext } from '../contexts/modal-context'
 
-export default function ProjectsContainer () {
-  const [modalProperties, setModalProperties] = useState({
-    visible: false,
-    props: {}
-  })
+export default function ProjectsContainer (props) {
+  const { setModalProperties } = useContext(ModalContext)
   return (
-    <section id="projects-container">
-       {modalProperties.visible && <ProjectsModal {...modalProperties.props} />}
-      <div id="projects-title">
-        <h1>Mergulhe no meu mundo</h1>
-      </div>
-      <div id="projects">
-        <ProjectsCarrosel setModalProperties={setModalProperties} />
-      </div>
-      <hr />
-    </section>
+    <>
+      <section id="projects-container">
+        <div id="projects-title">
+          <h1>Mergulhe no meu mundo</h1>
+        </div>
+        <div id="projects">
+          <ProjectsCarrosel setModalProperties={setModalProperties} />
+        </div>
+        <hr />
+      </section>
+    </>
   )
 }
