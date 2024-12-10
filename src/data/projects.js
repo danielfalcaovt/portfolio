8edafd3a1322aee8dc4370ba7494e4094a6c32e8
Daniel <danielfalcaoviktorio@gmail.com>
2024-12-10 14:43:44 -0300

    feat: new properties add to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,28 +4,48 @@ export const allProjects = [
     name: 'TransiScale',
     type: 'API',
     bg: 'project1.png',
-    link: 'https://github.com/Debug-Heroes/hackacont-transiscale-front',
-    video: 'https://youtu.be/gc0rbUS6ECk'
+    github: 'https://github.com/Debug-Heroes/hackacont-transiscale-front',
+    video: 'https://youtu.be/gc0rbUS6ECk',
+    technologies: ['nestjs', 'postgresql', 'react'],
+    description:
+      'Ferramenta que auxilia gestores na transição de escalas 6x1 para 4x3, simulando impactos operacionais e sugerindo ajustes automáticos nos turnos. O sistema considera carga horária, folgas obrigatórias e leis trabalhistas, garantindo eficiência, bem-estar dos colaboradores e continuidade das operações.'
   },
   {
     id: 2,
     name: 'GerenciaBook',
     type: 'API',
     bg: 'project4.png',
-    link: 'https://github.com/danielfalcaovt/GerenciaBook-UI'
+    github: 'https://github.com/danielfalcaovt/GerenciaBook-UI',
+    technologies: [
+      'typescript',
+      'nodejs',
+      'postgresql',
+      'react',
+      'jest',
+      'docker'
+    ],
+    description:
+      'Bem-vindo ao projeto Gerenciador de Empréstimo de Livros! Este é um sistema projetado para facilitar o gerenciamento de empréstimos de livros. O sistema permite autenticação de usuários, registro e operações CRUD (Criar, Ler, Atualizar, Excluir) em relação aos livros.'
   },
   {
     id: 3,
     name: 'Task Manager',
     type: 'API',
     bg: 'project3.png',
-    link: 'https://github.com/danielfalcaovt/TaskManager'
+    github: 'https://github.com/danielfalcaovt/TaskManager',
+    technologies: ['react', 'postgresql', 'typescript'],
+    description:
+      'O TaskPilot é um aplicativo de organização pessoal que ajuda a gerenciar tarefas, compromissos e notas. Ele oferece um calendário para visualizar e atribuir tarefas, uma caixa de notificações para lembretes e uma frase motivacional diária. O app também permite personalizar configurações e manter o controle de responsabilidades, visando aumentar a produtividade e simplificar a vida do usuário.'
   },
   {
     id: 4,
     name: 'MY BOOKSHELF',
     type: 'Website',
     bg: 'project2.png',
-    link: 'https://minha-bookshelf.vercel.app/'
+    github: 'https://github.com/danielfalcaovt/my-bookshelf',
+    deploy: 'https://minha-bookshelf.vercel.app/',
+    technologies: ['javascript', 'react'],
+    description:
+      'Aqui estão todos os livros que li em todos os meus dias na Terra e o que eu pensei sobre eles. Apenas lembre-se de que eu não sou um avaliador profissional de livros. Sou apenas um desenvolvedor e um cara curioso.'
   }
 ]
