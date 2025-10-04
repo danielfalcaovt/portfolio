ad673a0f632d24ef822b71e2a8ecd8f41b2c582c
ppsdev-daniel <146419346+danielfalcaovt@users.noreply.github.com>
2025-10-04 11:14:31 -0300

    fix: speed insight

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import RouteSetup from "./configuration/routes/RouteSetup";
 import { theme } from "./configuration/theme";
 import { useEffect, useState } from "react";
 import i18next from "i18next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App() {
   const [, setForceUpdate] = useState(false);
@@ -23,7 +22,6 @@ export default function App() {
   }, []);
   return (
     <>
-      <SpeedInsights />
       <MantineProvider theme={theme} defaultColorScheme="dark">
         <RouteSetup />
       </MantineProvider>
